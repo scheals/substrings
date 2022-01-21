92722965e61ea495a9f84a7c55647f1127a58a3a
scheals <piecoo@gmail.com>
2022-01-21 11:02:33 +0100

    Introduce another non-solution

--- substrings.rb
+++ substrings.rb
@@ -1,10 +1,18 @@
 def substrings(string, dictionary)
-  string.split(" ").reduce(Hash.new(0)) do |word, word_count|
-    word[word_count.downcase] += 1
-    word
+  string.split(" ").reduce(Hash.new(0)) do |word_count, word|
+    # p dictionary.select { |dictionary_word| word.downcase.include?(dictionary_word) }
+    substrings = dictionary.select { |dictionary_word| word.downcase.include?(dictionary_word) }
+    substrings = substrings.reduce(Hash.new(0)) do |hash, woot|
+      hash[woot] += 1
+      hash
+      hash
+    end
+    p substrings
+    word_count[substrings] += 1
+    word_count
   end
 end
 
-p substrings("Testing", ["Testing", "testing", "undefined", "filler"])
-p substrings("This this is a test testing a method.", ["this", "is", "a", "test", "method", "testing", "A", "filler", "ghost"])
+# p substrings("Testing", ["Testing", "testing", "undefined", "filler"])
+p substrings("This this is a test testing Testing a method...", ["this", "is", "a", "test", "method", "testing", "A", "filler", "ghost"])
 p substrings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
